eda282cdc01058543b444e4549bf3b3b9dd15255
mempler <me@mempler.de>
2018-05-14 18:24:08 +0200

    On Login, send Friendlist

--- packets/loginpermissions.go
+++ packets/loginpermissions.go
@@ -2,12 +2,11 @@ package packets
 
 import (
 	"git.gigamons.de/Gigamons/Kaoiji/constants"
-	"git.gigamons.de/Gigamons/Kaoiji/objects"
 )
 
 // LoginPermissions sends the "Client" permissions to the client.
-func (w *Writer) LoginPermissions(t *objects.Token) {
+func (w *Writer) LoginPermissions() {
 	p := NewPacket(constants.BanchoLoginPermissions)
-	p.SetPacketData(Int32(int32(t.Status.Info.ClientPerm)))
+	p.SetPacketData(Int32(int32(w._token.Status.Info.ClientPerm)))
 	w.Write(p.ToByteArray())
 }
