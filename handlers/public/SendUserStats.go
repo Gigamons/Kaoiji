75efbe7934a593170ce693c0915072985490a5e2
Mempler <me@mempler.de>
2018-06-07 10:57:45 +0200

    Fix Stuff.

--- handlers/public/SendUserStats.go
+++ handlers/public/SendUserStats.go
@@ -18,15 +18,16 @@ func SendUserStats(t *objects.Token, forced bool) []byte {
 	x.UserID = t.User.ID
 	x.Status = t.Status.Beatmap.Status
 	x.StatusText = t.Status.Beatmap.StatusText
-	x.CurrentMods = t.Status.Beatmap.CurrentMods
 	x.BeatmapChecksum = t.Status.Beatmap.BeatmapChecksum
+	x.CurrentMods = t.Status.Beatmap.CurrentMods
 	x.PlayMode = t.Status.Beatmap.PlayMode
-	x.Accuracy = helpers.CalculateAccuracy(t.Leaderboard.Count300, t.Leaderboard.Count100, t.Leaderboard.Count50, t.Leaderboard.CountMiss, 0, 0, 0)
-	x.TotalScore = uint64(t.Leaderboard.TotalScore)
-	x.PlayCount = int32(t.Leaderboard.Playcount)
-	x.PeppyPoints = t.Leaderboard.PeppyPoints
+	x.BeatmapID = t.Status.Beatmap.BeatmapID
 	x.RankedScore = uint64(t.Leaderboard.RankedScore)
+	x.Accuracy = float32(helpers.CalculateAccuracy(t.Leaderboard.Count300, t.Leaderboard.Count100, t.Leaderboard.Count50, t.Leaderboard.CountMiss, 0, 0, 0))
+	x.PlayCount = int32(t.Leaderboard.Playcount)
+	x.TotalScore = uint64(t.Leaderboard.TotalScore)
 	x.Rank = usertools.GetLeaderboardPosition(t.User, t.Status.Beatmap.PlayMode)
+	x.PeppyPoints = int16(t.Leaderboard.PeppyPoints)
 	w.SendUserStats(x)
 	return w.Bytes()
 }
